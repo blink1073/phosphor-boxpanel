2b6861314695a2c319daf26238892968bb4285f0
Steven Silvester <steven.silvester@ieee.org>
2015-12-05 09:44:14 -0600

    Add a test using stealjs

---
+++ test/karma.bootstrap.js
@@ -0,0 +1,15 @@
+window.__karma__.loaded = function() {
+  System.config({
+    paths: {
+        'test/build/*': 'test/build/*'
+    }
+  });
+  System.main = 'test/build/index.js';
+
+
+  steal.done().then(function(){
+    if(window.__karma__) {
+      window.__karma__.start();
+    }
+  });
+};
--- test/karma.conf.js
+++ test/karma.conf.js
@@ -1,10 +1,19 @@
 module.exports = function (config) {
   config.set({
-    frameworks: ['browserify', 'mocha'],
+    basePath: '..',
+    frameworks: ['mocha'],
     reporters: ['mocha'],
-    preprocessors: { 'build/index.js': ['browserify'] },
-    browserify: { debug: true, transform: ['browserify-css'] },
-    files: ['build/index.js'],
+    files: [
+            { pattern: 'lib/*.*', included: false },
+            { pattern: 'node_modules/steal/**/*.js', included: false },
+            { pattern: 'package.json', included: false },
+            { pattern: 'node_modules/**/package.json', included: false},
+            { pattern: 'node_modules/**/*.js', included: false },
+            { pattern: 'node_modules/**/*.css', included: false },
+            { pattern: 'test/build/index.*', included: false },
+            'node_modules/steal/steal.js',
+            'test/karma.bootstrap.js'
+            ],
     port: 9876,
     colors: true,
     singleRun: true,
