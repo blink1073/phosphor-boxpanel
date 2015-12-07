69ddfbf5ec253bec22b4a7aa9b201f3357a75e1c
Steven Silvester <steven.silvester@ieee.org>
2015-12-07 10:24:59 -0600

    Simplify karma conf

--- test/karma-cov.conf.js
+++ test/karma-cov.conf.js
@@ -6,11 +6,8 @@ module.exports = function (config) {
     reporters: ['mocha', 'coverage'],
     files: [
       { pattern: 'lib/*.*', included: false },
-      { pattern: 'node_modules/steal/**/*.js', included: false },
       { pattern: 'package.json', included: false },
-      { pattern: 'node_modules/**/package.json', included: false },
-      { pattern: 'node_modules/**/*.js', included: false },
-      { pattern: 'node_modules/**/*.css', included: false },
+      { pattern: 'node_modules/**/*.*', included: false },
       { pattern: 'test/build/index.*', included: false },
       'node_modules/steal/steal.js',
       'test/karma.bootstrap.js'
--- test/karma.conf.js
+++ test/karma.conf.js
@@ -5,11 +5,9 @@ module.exports = function (config) {
     reporters: ['mocha'],
     files: [
       { pattern: 'lib/*.*', included: false },
-      { pattern: 'node_modules/steal/**/*.js', included: false },
       { pattern: 'package.json', included: false },
-      { pattern: 'node_modules/**/package.json', included: false },
-      { pattern: 'node_modules/**/*.js', included: false },
-      { pattern: 'node_modules/**/*.css', included: false },
+      { pattern: 'node_modules/**/*.*', included: false },
+      { pattern: 'test/build/index.*', included: false },
       { pattern: 'test/build/index.*', included: false },
       'node_modules/steal/steal.js',
       'test/karma.bootstrap.js'
