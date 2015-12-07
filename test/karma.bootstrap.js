ee7249aa62e813918c6ae892e79b3257d21f2bf2
Steven Silvester <steven.silvester@ieee.org>
2015-12-07 10:01:43 -0600

    Fix formatting

--- test/karma.bootstrap.js
+++ test/karma.bootstrap.js
@@ -1,7 +1,7 @@
 window.__karma__.loaded = function() {
   System.main = 'test/build/index';
 
-  steal.done().then(function(){
+  steal.done().then(function() {
     if (window.__karma__) {
       window.__karma__.start();
     }
