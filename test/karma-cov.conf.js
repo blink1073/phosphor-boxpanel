1182bda2982a9edcf9a18a820c9ee350b2e6bcad
Steven Silvester <steven.silvester@ieee.org>
2015-12-07 09:31:59 -0600

    Update coverage script

--- test/karma-cov.conf.js
+++ test/karma-cov.conf.js
@@ -1,14 +1,20 @@
 module.exports = function (config) {
   config.set({
+    basePath: '..',
     browsers: ['Firefox'],
-    frameworks: ['browserify', 'mocha'],
+    frameworks: ['mocha'],
     reporters: ['mocha', 'coverage'],
-    preprocessors: { 'build/index.js': ['browserify'] },
-    browserify: {
-      debug: true,
-      transform: ['browserify-css', 'browserify-istanbul']
-    },
-    files: ['build/index.js'],
+    files: [
+      { pattern: 'lib/*.*', included: false },
+      { pattern: 'node_modules/steal/**/*.js', included: false },
+      { pattern: 'package.json', included: false },
+      { pattern: 'node_modules/**/package.json', included: false},
+      { pattern: 'node_modules/**/*.js', included: false },
+      { pattern: 'node_modules/**/*.css', included: false },
+      { pattern: 'test/build/index.*', included: false },
+      'node_modules/steal/steal.js',
+      'test/karma.bootstrap.js'
+    ],
     coverageReporter: {
       reporters : [
         { 'type': 'text' },
